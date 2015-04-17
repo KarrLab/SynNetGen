ad621d3b7c2eb53c8b80dc76bce617ec24d1c5be
Jonathan Karr <jonrkarr@gmail.com>
2015-04-17 19:54:00 -0400

    creating BoolNet class

---
+++ src/+synnetgen/+boolnet/BoolNet.m
@@ -0,0 +1,444 @@
+%Represents boolean network
+%- Methods
+%  - copy
+%  - isequal, eq
+%  - disp, print, plot
+%- Static methods
+%  - generate: from one of several distributions
+%  - import: from one of several file formats
+%  - export: to one of several file formats
+%
+%@author  Jonathan Karr, karr@mssm.edu
+%@date    2015-04-15
+classdef BoolNet < synnetgen.Model
+    properties (SetAccess = protected)
+        nodes
+        rules
+    end
+    
+    methods
+        function this = BoolNet(nodes, rules)
+            %Construct Boolean network
+            
+            if nargin < 1
+                nodes = repmat(struct('id', '', 'label', ''), 0, 1);
+            end
+            if nargin < 2
+                rules = repmat({''}, numel(nodes), 1);
+            end
+            
+            this.setNodesAndRules(nodes, rules);
+        end
+    end
+    
+    methods
+        function this = setNodesAndRules(this, nodes, rules)
+            %Set nodes and rules
+            
+            validateattributes(nodes, {'struct'}, {'column'});
+            if ~isequal(sort(fieldnames(nodes)), sort({'id'; 'label'}))
+                throw(MException('SynNetGen:InvalidArgument', 'Nodes must be struct with two fields id and label'));
+            end
+            if ~all(cellfun(@this.isValidNodeId, {nodes.id}))
+                throw(MException('SynNetGen:InvalidArgument', 'Names must be non-empty strings'));
+            end
+            if ~all(cellfun(@this.isValidNodeLabel, {nodes.label}))
+                throw(MException('SynNetGen:InvalidArgument', 'Labels must be strings'));
+            end
+            
+            if numel(nodes) ~= numel(rules) || ~this.areRulesValid(nodes, rules)
+                throw(MException('SynNetGen:InvalidArgument', 'Invalid rules'));
+            end
+            
+            this.nodes = nodes;
+            this.rules = rules;
+        end
+        
+        function this = addNode(this, id, label)
+            %Adds node with id and label to graph
+            
+            %% validate arguments
+            
+            %id is non-empty unique string
+            if ~this.isValidNodeId(id) || any(strcmp({this.nodes.id}, id))
+                throw(MException('SynNetGen:InvalidNodeName', 'Invalid node id ''%s''', id));
+            end
+            
+            %label is string
+            if ~this.isValidNodeLabel(label)
+                throw(MException('SynNetGen:InvalidNodeLabel', 'Invalid node label ''%s''', label));
+            end
+            
+            %% add node to network
+            this.nodes = [
+                this.nodes
+                struct('id', id, 'label', label)
+                ];
+            this.rules = [
+                this.rules
+                {''}
+                ];
+        end
+        
+        function this = setRule(this, id, rule)
+            %set rules
+            
+            %validate arguments
+            if ~this.isValidNodeId(id) || ~any(strcmp(id, {this.nodes.id}))
+                throw(MException('SynNetGen:InvalidArgument', 'No node with id'));
+            end
+            
+            if ~this.areRulesValid(this.nodes, {rule})
+                throw(MException('SynNetGen:InvalidArgument', 'Invalid rules'));
+            end
+            
+            %set rules
+            iNode = find(strcmp(id, {this.nodes.id}), 1, 'first');
+            this.rules{iNode} = rule;
+        end
+        
+        function this = setRules(this, rules)
+            %set rules
+            
+            %validate arguments
+            if numel(this.nodes) ~= numel(rules) || ~this.areRulesValid(this.nodes, rules)
+                throw(MException('SynNetGen:InvalidArgument', 'Invalid rules'));
+            end
+            
+            %set rules
+            this.rules = rules;
+        end
+        
+        function [truthTables, edges] = getTruthTables(this, varargin)
+            %Calculates truth tables for rules in matrix [nNodes, 2^kmax]
+            %and unsigned, directed graph
+            
+            %parse arguments
+            ip = inputParser();
+            ip.addParameter('simplify', false, @islogical)
+            ip.parse(varargin{:});
+            simplify = ip.Results.simplify;
+            
+            %calculate truth tables
+            edges = zeros(numel(this.nodes));
+            truthTables = zeros(numel(this.nodes), 0);
+            for iNode = 1:numel(this.nodes)
+                [truthTable, edges(:, iNode)] = this.getTruthTable(this.nodes(iNode).id, 'simplify', simplify);
+                truthTables = [truthTables NaN(numel(this.nodes), size(truthTable, 2) - size(truthTables, 2))];
+                truthTables(iNode, 1:numel(truthTable)) = truthTable;
+            end
+        end
+        
+        function [truthTable, edges] = getTruthTable(this, nodeId, varargin)
+            %Calculates truth table for rule in vector [1 2^k]
+            %and unsigned, directed graph
+            
+            %parse arguments
+            iNode = find(strcmp(nodeId, {this.nodes.id}), 1, 'first');
+            if isempty(nodeId)
+                throw(MException('SynNetGen:InvalidArgument', 'No node with id ''%s''', nodeId));
+            end
+            
+            ip = inputParser();
+            ip.addParameter('simplify', false, @islogical)
+            ip.addParameter('idFrom', unique(regexpi(this.rules{iNode}, '([a-z][a-z0-9_]*)', 'match')), @(x) iscell(x) && all(ismember(x, {this.nodes.id})))
+            ip.parse(varargin{:});
+            simplify = ip.Results.simplify;
+            idFrom = ip.Results.idFrom;
+            
+            %exit if no regulators
+            if isempty(idFrom)
+                edges = zeros(numel(this.nodes), 1);
+                truthTable = zeros(1, 0);
+                return;
+            end
+            
+            %calculate edges
+            edges = ismember({this.nodes.id}', idFrom);
+            
+            %calculate truth tables
+            truthTable = NaN(1, 2^max(sum(edges ~= 0)));
+            rule = regexprep(this.rules{iNode}, '([a-z][a-z0-9_]*)', 'nodeValsStr.$1', 'ignorecase');
+            
+            nodeValsStr = struct();
+            for i = 1:numel(this.nodes)
+                nodeValsStr.(this.nodes(i).id) = true;
+            end
+            
+            nodeValsArr = false(2^numel(idFrom), numel(idFrom));
+            
+            for j = 1:2^numel(idFrom)
+                nodeValsArr(j, :) = dec2base(j - 1, 2, numel(idFrom)) == '1';
+                for k = 1:numel(idFrom)
+                    nodeValsStr.(idFrom{k}) = nodeValsArr(j, k);
+                end
+                truthTable(1, j) = eval(rule);
+            end
+            
+            %determine if any inputs have no effective
+            isEffectiveInput = false(size(idFrom));
+            for j = 1:numel(idFrom)
+                isEffectiveInput(j) = ~isequal(truthTable(1, nodeValsArr(:, j) == 1), truthTable(1, nodeValsArr(:, j) == 0));
+            end
+            
+            %simplify
+            if simplify && ~all(isEffectiveInput)
+                [truthTable, edges] = this.getTruthTable(nodeId, 'simplify', simplify, 'idFrom', idFrom(isEffectiveInput));
+            end
+        end
+        
+        function edges = getEdges(this)
+            %Calculates signed, directed graph
+            
+            edges = zeros(numel(this.nodes));
+            for iNode = 1:numel(this.nodes)
+                edges(:, iNode) = this.getNodeEdges(this.nodes(iNode).id);
+            end
+            
+            [~, simplifiedEdges] = this.getTruthTables('simplify', true);
+            edges(simplifiedEdges == 0) = 0;
+        end
+        
+        function edges = getNodeEdges(this, nodeId)
+            %Calculates signed, directed graph for one node
+            
+            %parse arguments
+            iNode = find(strcmp(nodeId, {this.nodes.id}), 1, 'first');
+            if isempty(nodeId)
+                throw(MException('SynNetGen:InvalidArgument', 'No node with id ''%s''', nodeId));
+            end
+            
+            %eliminate spaces
+            rule = strrep(this.rules{iNode}, ' ', '');
+            
+            %calculate sense at each position
+            sense = false(size(rule));
+            depth = [];
+            senseDepth = true;
+            currDepth = 0;
+            for i = 1:numel(rule)
+                switch rule(i)
+                    case '('
+                        currDepth = currDepth + 1;
+                        if i > 1 && rule(i - 1) == '~'
+                            if numel(senseDepth) == currDepth
+                                senseDepth = [senseDepth ~senseDepth(currDepth)];
+                            else
+                                senseDepth(currDepth+1) = ~senseDepth(currDepth);
+                            end
+                        else
+                            if numel(senseDepth) == currDepth
+                                senseDepth = [senseDepth senseDepth(currDepth)];
+                            else
+                                senseDepth(currDepth+1) = senseDepth(currDepth);
+                            end
+                        end
+                    case ')'
+                        currDepth = currDepth - 1;
+                end
+                
+                if currDepth < 0
+                    throw(MException())
+                end
+                depth = [depth currDepth];
+                
+                if i > 1 && rule(i-1) == '~'
+                    sense(i) = ~senseDepth(currDepth+1);
+                else
+                    sense(i) = senseDepth(currDepth+1);
+                end
+            end
+            sense = 2 * sense - 1;
+            
+            %calculate edge senses
+            edges = zeros(numel(this.nodes), 1);
+            [nodeIds, nodeStarts] = regexpi(rule, '([a-z]\w*)', 'match', 'start');
+            [~, nodeIdxs] = ismember(nodeIds, {this.nodes.id});
+            for i = 1:numel(nodeIdxs)
+                switch edges(nodeIdxs(i))
+                    case 0
+                        edges(nodeIdxs(i)) = sense(nodeStarts(i));
+                    case {-1, 1}
+                        if edges(nodeIdxs(i)) ~= sense(nodeStarts(i))
+                            edges(nodeIdxs(i)) = NaN;
+                        end
+                end
+            end
+        end
+    end
+    
+    methods (Static)
+        function result = areRulesValid(nodes, rules)
+            %Check if rules are valid
+            
+            validateattributes(rules, {'cell'}, {'column'});
+            if ~all(cellfun(@(rule) ischar(rule) && isrow(rule), rules))
+                result = false;
+                return;
+            end
+            
+            nodeVals = struct();
+            for iNode = 1:numel(nodes)
+                nodeVals.(nodes(iNode).id) = true;
+            end
+            for iRule = 1:numel(rules)
+                if isempty(regexpi(rules{iRule}, '^((~?\()?~?[a-z]\w*( ?(\|\||\&\&) ?(~?\()?~?[a-z]\w*\)?)*)?$'))
+                    result = false;
+                    return;
+                end
+                
+                try
+                    rule = regexprep(rules{iRule}, '([a-z0-9_]*)', 'nodeVals.$1', 'ignorecase');
+                    [~] = eval(rule);
+                catch
+                    result = false;
+                    return;
+                end
+            end
+            
+            result = true;
+        end
+    end
+    
+    methods
+        function tf = isequal(this, that)
+            %that is graph
+            if ~isa(that, class(this))
+                tf = false;
+                return;
+            end
+            
+            %same number nodes
+            if ~isequal(numel(this.nodes), numel(that.nodes))
+                tf = false;
+                return;
+            end
+            
+            %same node names and labels
+            [nodeNameLabelsThis, iRowsThis] = sortrows([{this.nodes.id}' {this.nodes.label}']);
+            [nodeNameLabelsThat, iRowsThat] = sortrows([{that.nodes.id}' {that.nodes.label}']);
+            if ~isequal(nodeNameLabelsThis, nodeNameLabelsThat)
+                tf = false;
+                return;
+            end
+            
+            %same edges
+            thisSorted = synnetgen.boolnet.BoolNet(this.nodes(iRowsThis), this.rules(iRowsThis));
+            thatSorted = synnetgen.boolnet.BoolNet(that.nodes(iRowsThat), that.rules(iRowsThat));
+            
+            if ~isequal(thisSorted.getEdges(), thatSorted.getEdges())
+                tf = false;
+                return;
+            end
+            
+            thisTruthTables = thisSorted.getTruthTables();
+            thatTruthTables = thatSorted.getTruthTables();
+            if ~all(all(thisTruthTables == thatTruthTables | (isnan(thisTruthTables) & isnan(thatTruthTables))))
+                tf = false;
+                return;
+            end
+            
+            %if all tests pass, return true
+            tf = true;
+        end
+    end
+    
+    methods
+        function this = clear(this)
+            %clear nodes and rules
+            
+            this.nodes = repmat(struct('id', [], 'label', []), 0, 1);
+            this.rules = cell(0, 1);
+        end
+        
+        function that = copy(this)
+            %Create copy of network
+            
+            that = synnetgen.boolnet.BoolNet(this.nodes, this.rules);
+        end
+    end
+    
+    methods
+        function str = print(this)
+            %Display network in command window
+            
+            str = cell(0, 1);
+            
+            str = [str
+                sprintf('Boolean network with %d nodes', numel(this.nodes))
+                ];
+            
+            str = [
+                str
+                sprintf('  Nodes:')
+                ];
+            for iNode = 1:numel(this.nodes)
+                str = [
+                    str
+                    sprintf('    %s: %s', this.nodes(iNode).id, this.nodes(iNode).label)
+                    ];
+            end
+            
+            str = [
+                str
+                sprintf('  Rules:')
+                ];
+            for iNode = 1:numel(this.nodes)
+                str = [
+                    str
+                    sprintf('    %s: %s', this.nodes(iNode).id, this.rules{iNode})
+                    ];
+            end
+        end
+        
+        function figHandle = plot(this, varargin)
+            %Generates plot of network
+            %  Options:
+            %  - layout: see graphviz4matlab\layouts
+            %  - nodeColors
+            
+            graph = synnetgen.graph.Graph(this.nodes(), this.getEdges());
+            figHandle = graph.plot(varargin{:});
+        end
+    end
+    
+    methods
+        function result = generate(this, extId, varargin)
+            %Generates random network using various generators. See
+            %synnetgen.boolnet.generator for supported algorithms and their
+            %options.
+            
+            result = synnetgen.extension.ExtensionRunner.run('synnetgen.boolnet.generator', extId, this, varargin{:});
+        end
+        
+        function result = transform(this, extId, varargin)
+            %Transforms network using various transform. See
+            %synnetgen.boolnet.transform for supported algorithms and their
+            %options.
+            
+            result = synnetgen.extension.ExtensionRunner.run('synnetgen.boolnet.transform', extId, this, varargin{:});
+        end
+        
+        function result = convert(this, extId, varargin)
+            %Converts network to other types of models using various
+            %algorithms. See synnetgen.boolnet.transform for supported
+            %algorithms and their options.
+            
+            result = synnetgen.extension.ExtensionRunner.run('synnetgen.boolnet.convert', extId, this, varargin{:});
+        end
+        
+        function result = import(this, extId, varargin)
+            %Imports networks from various formats. See synnetgen.boolnet.importer
+            %for supported formats.
+            
+            result = synnetgen.extension.ExtensionRunner.run('synnetgen.boolnet.importer', extId, this, varargin{:});
+        end
+        
+        function result = export(this, extId, varargin)
+            %Exports network to various formats. See synnetgen.boolnet.exporter
+            %for supported formats.
+            
+            result = synnetgen.extension.ExtensionRunner.run('synnetgen.boolnet.exporter', extId, this, varargin{:});
+        end
+    end
+end
---
+++ src_test/+synnetgen/BoolNetTest.m
@@ -0,0 +1,293 @@
+%Tests boolean network class and associated extensions
+%
+%@author  Jonathan Karr, karr@mssm.edu
+%@date    2015-04-15
+classdef BoolNetTest < matlab.unittest.TestCase
+    methods (Test)
+        function testNewNetwork(this)
+            n = synnetgen.boolnet.BoolNet();
+        end
+        
+        function testAddNode(this)
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            
+            this.verifyEqual(n.rules, {''; ''; ''});
+        end
+        
+        function testSetRule(this)
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('b', 'a');
+            this.verifyEqual(n.rules, {''; 'a'; ''});
+        end
+        
+        function testAreRulesValid(this)
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            
+            this.verifyTrue(n.areRulesValid(n.nodes, {'a'}));
+            this.verifyTrue(n.areRulesValid(n.nodes, {'a || b'}));
+            this.verifyFalse(n.areRulesValid(n.nodes, {'A'}));
+            this.verifyFalse(n.areRulesValid(n.nodes, {'d'}));
+        end
+        
+        function testSetNodesAndRules(this)
+            n = synnetgen.boolnet.BoolNet();
+            
+            nodes = [
+                struct('id', 'a', 'label', 'A')
+                struct('id', 'b', 'label', 'B')
+                struct('id', 'c', 'label', 'C')
+                ];
+            rules = {'b || c'; 'a && c'; '~a && b'};
+            n.setNodesAndRules(nodes, rules);
+            
+            this.verifyEqual(n.nodes, nodes);
+            this.verifyEqual(n.rules, rules);
+        end
+        
+        function testSetRules(this)
+            n = synnetgen.boolnet.BoolNet();
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            
+            rules = {'b || c'; 'a && c'; '~a && b'};
+            n.setRules(rules);
+            
+            this.verifyEqual(n.rules, rules);
+        end
+        
+        function testGetTruthTables(this)
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('a', 'b || c');
+            n.setRule('b', 'c && a');
+            n.setRule('c', '~(a || b) && c');
+            
+            truthTables = n.getTruthTables();
+            this.verifySize(truthTables, [3 8]);
+            this.verifyEqual(sum(~isnan(truthTables), 2), [4 4 8]');
+            this.verifyEqual(nansum(truthTables, 2), [3; 1; 1]);
+            this.verifyEqual(truthTables, [
+                0 1 1 1 NaN NaN NaN NaN
+                0 0 0 1 NaN NaN NaN NaN
+                0 1 0 0 0   0   0   0
+                ]);
+        end
+        
+        function testGetTruthTablesSimplified(this)
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('a', 'b || c');
+            n.setRule('b', 'a || ~a');
+            n.setRule('c', '~(a || b) && c');
+            
+            truthTables = n.getTruthTables('simplify', true);
+            this.verifySize(truthTables, [3 8]);
+            this.verifyEqual(sum(~isnan(truthTables), 2), [4 0 8]');
+            this.verifyEqual(nansum(truthTables, 2), [3; 0; 1]);
+            this.verifyEqual(truthTables, [
+                0   1   1   1   NaN NaN NaN NaN
+                NaN NaN NaN NaN NaN NaN NaN NaN
+                0   1   0   0   0   0   0   0
+                ]);
+        end
+        
+        function testGetEdges(this)
+            %ex 1
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('a', 'b || c');
+            n.setRule('b', 'c && a');
+            n.setRule('c', '~(a || b) && c');
+            
+            edges = n.getEdges();
+            this.verifyEqual(edges, [
+                0 1 -1
+                1 0 -1
+                1 1  1
+                ]);
+            
+            %ex 2
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('a', 'b || c');
+            n.setRule('b', 'a || ~a');
+            n.setRule('c', '~(a || b) && c');
+            
+            edges = n.getEdges();
+            this.verifyEqual(edges, [
+                0 0 -1
+                1 0 -1
+                1 0 1
+                ]);
+            
+            %ex 3
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.addNode('d', 'D');
+            n.addNode('e', 'E');
+            n.addNode('f', 'F');
+            n.setRule('a', '~(a || b || c) && (d || e || f)');
+            n.setRule('b', '~d && (a || b)');
+            n.setRule('c', '(c || b)');
+            n.setRule('d', '~(c || b)');
+            n.setRule('e', '~(a && b) && (c || d)');
+            n.setRule('f', '~(a && b) && (a && c)');
+            
+            edges = n.getEdges();
+            this.verifyEqual(edges, [
+                -1  1  0  0 -1 NaN
+                -1  1  1 -1 -1  -1
+                -1  0  1 -1  1   1
+                 1 -1  0  0  1   0
+                 1  0  0  0  0   0
+                 1  0  0  0  0   0
+                ]);
+        end
+        
+        function testIsequal(this)
+            n = synnetgen.boolnet.BoolNet();
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('a', 'b');
+            n.setRule('b', 'c && a');
+            n.setRule('c', 'a');
+            
+            m = synnetgen.boolnet.BoolNet();
+            m.addNode('b', 'B');
+            m.addNode('a', 'A');
+            m.addNode('c', 'C');
+            m.setRule('a', 'b');
+            m.setRule('b', '~(~a || ~c)');
+            m.setRule('c', 'a');
+            this.verifyEqual(n, m);
+            
+            o = synnetgen.boolnet.BoolNet();
+            o.addNode('b', 'B');
+            o.addNode('a', 'A');
+            o.addNode('c', 'C');
+            o.addNode('d', 'D');
+            o.setRule('a', 'b');
+            o.setRule('b', '~(~a || ~c)');
+            o.setRule('c', 'a');
+            this.verifyNotEqual(o, m);
+            
+            p = synnetgen.boolnet.BoolNet();
+            p.addNode('b', 'B');
+            p.addNode('a', 'A');
+            p.addNode('c', 'C');
+            p.setRule('a', 'b');
+            p.setRule('b', '~(a || ~c)');
+            p.setRule('c', 'a');
+            this.verifyNotEqual(p, m);
+        end
+        
+        function testClear(this)
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('a', 'b');
+            n.setRule('b', 'c');
+            n.setRule('c', 'a');
+            n.clear();
+            
+            this.verifyEqual(n.nodes, repmat(struct('id', '', 'label', ''), 0, 1));
+            this.verifyEqual(n.rules, cell(0, 1));
+        end
+        
+        function testCopy(this)
+            n = synnetgen.boolnet.BoolNet();
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('a', 'b');
+            n.setRule('b', 'c');
+            n.setRule('c', 'a');
+            
+            m = n.copy();
+            
+            this.verifyEqual(m.nodes, n.nodes);
+            this.verifyEqual(m.rules, n.rules);
+        end
+        
+        function testDisp(this)
+            n = synnetgen.boolnet.BoolNet();
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('a', 'b');
+            n.setRule('b', 'c');
+            n.setRule('c', 'a');
+            
+            n
+        end
+        
+        function testPrint(this)
+            n = synnetgen.boolnet.BoolNet();
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.setRule('a', 'b');
+            n.setRule('b', 'c');
+            n.setRule('c', 'a');
+            
+            this.verifyTrue(iscell(n.print()))
+            this.verifyTrue(all(cellfun(@ischar, n.print())));
+        end
+        
+        function testPlot(this)
+            n = synnetgen.boolnet.BoolNet();
+            
+            n.addNode('a', 'A');
+            n.addNode('b', 'B');
+            n.addNode('c', 'C');
+            n.addNode('d', 'D');
+            n.addNode('e', 'E');
+            n.addNode('f', 'F');
+            n.setRule('a', '~(a || b || c) && (d || e || f)');
+            n.setRule('b', '~d && (a || b)');
+            n.setRule('c', '(c || b)');
+            n.setRule('d', '~(c || b)');
+            n.setRule('e', '~(a && b) && (c || d)');
+            n.setRule('f', '~(a && b) && (a && c)');
+            
+            h = n.plot();
+            close(h);
+        end
+        
+        %generate
+        %transform
+        %export
+        %import
+    end
+end
