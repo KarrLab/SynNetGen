c68d639ca75f61c60d7860cb8fc0d4eab772827e
Jonathan Karr <jonrkarr@gmail.com>
2015-04-18 14:02:30 -0400

    adding SBML exporter to test

--- src_test/+synnetgen/+extension/ExtensionRunnerTest.m
+++ src_test/+synnetgen/+extension/ExtensionRunnerTest.m
@@ -11,6 +11,7 @@ classdef ExtensionRunnerTest < matlab.unittest.TestCase
             %print
             synnetgen.extension.ExtensionRunner.get('synnetgen.boolnet.converter', 'graph');
             synnetgen.extension.ExtensionRunner.get('synnetgen.boolnet.exporter', 'R-BoolNet');
+            synnetgen.extension.ExtensionRunner.get('synnetgen.boolnet.exporter', 'SBML');
             synnetgen.extension.ExtensionRunner.get('synnetgen.boolnet.importer', 'R-BoolNet');
             
             synnetgen.extension.ExtensionRunner.get('synnetgen.graph.converter', 'boolnet');
